2edd266de8875e3173fe5ae7ebc1b6828b8c0a67
kunj3740 <kunjdave694@gmail.com>
2025-07-02 18:22:53 +0530

    working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ import LandingPage from "./pages/LandingPage";
 import AppBar from "./components/AppBar";
 import ProjectManager from "./pages/MyProjects";
 import { Editor } from "./pages/Editor";
-// import { Project } from "./pages/Project";
+import { Project } from "./pages/Project";
+import ThisWillBreakBuild from './components/DefinitelyNotReal';
+
 
 function App() {
   const location = useLocation()
