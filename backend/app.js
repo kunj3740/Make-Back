50c8553b83f36dc9de662d48ea9f5d632aa72966
kunj3740 <kunjdave694@gmail.com>
2025-07-02 17:42:18 +0530

    ci tested

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 })
 .then(() => {
   console.log('Connected to MongoDB')
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  // app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
 .catch((err) => {
   console.error('MongoDB connection error:', err.message)
