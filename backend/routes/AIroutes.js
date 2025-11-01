455312ba251d3f3133c173d831b4eadf14962437
kunj3740 <kunjdave694@gmail.com>
2025-11-01 15:47:53 +0530

    debuggingsss

--- backend/routes/AIroutes.js
+++ backend/routes/AIroutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
-// const { generateDatabaseSchema, chatWithAI, generateAPI, generateAPISuggestions, createAPIFromSuggestionName } = require('../controllers/AIController');
-// const authMiddleware = require('../middleware/auth');
+const { generateDatabaseSchema, chatWithAI, generateAPI, generateAPISuggestions,
+     createAPIFromSuggestionName } = require('../controllers/AIController');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
  
 // POST /api/ai/generate-schema
-// router.post('/generate-schema', generateDatabaseSchema);
+router.post('/generate-schema', generateDatabaseSchema);
 
-// router.post('/generate/apis/suggestion',authMiddleware , generateAPISuggestions);
+router.post('/generate/apis/suggestion',authMiddleware , generateAPISuggestions);
 
-// router.post('/generate/apis/generation',authMiddleware , createAPIFromSuggestionName);
+router.post('/generate/apis/generation',authMiddleware , createAPIFromSuggestionName);
 
-// // router.post('/generate/apis/creation',authMiddleware , generateAndCreateAPIs);
+// router.post('/generate/apis/creation',authMiddleware , generateAndCreateAPIs);
 
-// router.post('/chat', chatWithAI);
+router.post('/chat', chatWithAI);
 
-// router.post('/generate-api', generateAPI);
+router.post('/generate-api', generateAPI);
 
 module.exports = router;
