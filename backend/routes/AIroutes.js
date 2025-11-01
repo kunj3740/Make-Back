eefdc86d90c23037ce0f37b7a2af4f53737b4568
kunj3740 <kunjdave694@gmail.com>
2025-11-01 15:41:11 +0530

    debuggingsss

--- backend/routes/AIroutes.js
+++ backend/routes/AIroutes.js
@@ -5,16 +5,16 @@ const authMiddleware = require('../middleware/auth');
 const router = express.Router();
  
 // POST /api/ai/generate-schema
-router.post('/generate-schema', generateDatabaseSchema);
+// router.post('/generate-schema', generateDatabaseSchema);
 
-router.post('/generate/apis/suggestion',authMiddleware , generateAPISuggestions);
+// router.post('/generate/apis/suggestion',authMiddleware , generateAPISuggestions);
 
-router.post('/generate/apis/generation',authMiddleware , createAPIFromSuggestionName);
+// router.post('/generate/apis/generation',authMiddleware , createAPIFromSuggestionName);
 
-// router.post('/generate/apis/creation',authMiddleware , generateAndCreateAPIs);
+// // router.post('/generate/apis/creation',authMiddleware , generateAndCreateAPIs);
 
-router.post('/chat', chatWithAI);
+// router.post('/chat', chatWithAI);
 
-router.post('/generate-api', generateAPI);
+// router.post('/generate-api', generateAPI);
 
 module.exports = router;
