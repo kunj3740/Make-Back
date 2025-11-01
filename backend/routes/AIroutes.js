7aab5d0e5afb9e7103289ab081b241793fe3ee30
kunj3740 <kunjdave694@gmail.com>
2025-11-01 15:43:21 +0530

    debuggingsss

--- backend/routes/AIroutes.js
+++ backend/routes/AIroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { generateDatabaseSchema, chatWithAI, generateAPI, generateAPISuggestions, createAPIFromSuggestionName } = require('../controllers/AIController');
-const authMiddleware = require('../middleware/auth');
+// const { generateDatabaseSchema, chatWithAI, generateAPI, generateAPISuggestions, createAPIFromSuggestionName } = require('../controllers/AIController');
+// const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
  
